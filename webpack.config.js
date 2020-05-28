558c863c3fd18141e6f8826d7c28c76582847986
meluotii <alenayevreiskaya@gmail.com>
2020-05-28 13:55:07 +0300

    add hw

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,8 @@ module.exports = {
                     path.resolve(__dirname, 'src/scss')
                 ],
                 use: [
-                    // Creates `style` nodes from JS strings
                     'style-loader',
-                    // Translates CSS into CommonJS
                     'css-loader',
-                    // Compiles Sass to CSS
                     'sass-loader',
                 ],
             },
